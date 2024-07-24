373202bee95e20ac67af3d87d8e35d1303d79a9e
dhanishkumar3003 <dhanishkumar3003@gmail.com>
2024-07-24 10:29:48 +0530

    Add files via upload

---
+++ Python/Datatypes/decision_1.py
@@ -0,0 +1,24 @@
+num= int(input('enter your number: '))
+if(num%2==0):
+    print(f'{num} is even')
+else:
+    print(f'{num} is odd')
+
+print()
+
+# days = int(input('enter the number between 1 to 7'))
+
+# if(days == 1):
+#     print('monday : we will be learning datatypes')
+# elif(days == 2):
+#     print('tuesday : we will be learning datatypes')
+# elif(days == 3):
+#     print('wednesday : we will be learning datatypes')
+# elif(days == 4):
+#     print('thursday : we will be learning datatypes')
+# elif(days == 5):
+#     print('friday : we will be learning datatypes')
+# elif(days == 6):
+#     print('saturday : we will be learning datatypes')
+# elif(days == 7):
+#     print('sunday : we will be learning datatypes')
---
+++ Python/Module/arithmatic.py
@@ -0,0 +1,11 @@
+def addition(value_1,value_2):
+    return value_1+value_2
+
+def subtraction(value_1,value_2):
+    return  value_1-value_2
+
+def multiplication(value_1,value_2):
+    return  value_1*value_2
+
+def division(value_1,value_2):
+    return value_1/value_2
